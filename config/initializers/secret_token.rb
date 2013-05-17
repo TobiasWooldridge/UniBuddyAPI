360b01694ebaf95155772b865ee92f931f2bd957
Tobias Wooldridge <tobias@wooldridge.id.au>
2013-05-17 10:41:01 +0930

    Created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlindersAPI2::Application.config.secret_token = 'fa9202bda09a11f688a104363e34cc7ba17dd89fbd84c29191d85132b8cc993bf452d96ad94758b1a0679b68d928fac6f6c6ff6ad6632afc0589c54381a1fe3f'
