24717ae14a1c6acaa21d918202438087182e2d21
Tobias Wooldridge <tobias@wooldridge.id.au>
2013-08-31 22:02:59 +1000

    Modified term dates API page to only show this year's dates

--- app/models/term_dates.rb
+++ app/models/term_dates.rb
@@ -1,9 +1,15 @@
 class TermDates < ActiveRecord::Base
-
-
   class << self
     def current_week
-      TermDates.where("now() BETWEEN starts_at AND ends_at").first or ""
+      TermDates.where("? BETWEEN starts_at AND ends_at", Time.now).first or ""
+    end
+
+    def this_year
+      for_year Date.today.at_beginning_of_year
+    end
+
+    def for_year year
+      TermDates.where("starts_at BETWEEN ? AND ?", year.at_beginning_of_year, year.at_end_of_year) or ""
     end
   end
 end
