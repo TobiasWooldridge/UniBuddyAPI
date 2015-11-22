f699dcda975c275b6c9799a3fbe3be16faf6d056
Tobias Wooldridge <tobias@wooldridge.id.au>
2015-11-22 15:45:06 -0800

    Fixes whenever multiple jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,13 +7,13 @@
 set :output, "log/cron.log"
 
 every :day, :at => '3:00am' do
-  rake 'flinders_timetables:update_json[2016]' 'institution:update_semesters[true]'
+  rake 'flinders_timetables:update_json[2016] institution:update_semesters[true]'
 end
 
 every :day, :at => '3:30am' do
-  rake 'adelaide_timetables:update[2016]' 'institution:update_semesters[true]'
+  rake 'adelaide_timetables:update[2016] institution:update_semesters[true]'
 end
 
 every :day, :at => '4:00am' do
-  rake 'unisa_timetables:update' 'institution:update_semesters[true]'
+  rake 'unisa_timetables:update institution:update_semesters[true]'
 end
